7fe2a305bbf78231bcd812eb02979986d3b45a6a
ParthSankpal <parth.sankpal47@gmail.com>
2023-05-01 23:46:34 +0530

    API Worked

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -22,7 +22,7 @@ router.route("/").get((req,res)=>{
 router.route('/').post(async(req,res)=>{
     try {
         const {prompt}= req.body;
-        const aiResponse = await apenai.createImage({
+        const aiResponse = await openai.createImage({
         prompt,
         n:1,
         size:'1024x1024',
